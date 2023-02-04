7eece61dd5672a0cdeb474cf1cbc6b7d605cd221
Norik <norik.khachatryan@goya.am>
2023-02-04 21:02:58 +0400

    Front end Version:1.0.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+const productsUrl = 'http://localhost:8000/products';
+const ordersUrl = 'http://localhost:8000/orders';
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(productsUrl);
+      const products = await res.json();
+      setProducts(products);
+    }
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(ordersUrl);
+      const orders = await res.json();
+      setCart(orders);
+    }
+
+    fetchData();
+  }, []);
+
+  const handleAddToCart = async (productId) => {
+    const res = await fetch(`${productsUrl}/${productId}`, {
+      method: 'POST',
+    });
+    console.log(res)
+    const updated = await fetch(ordersUrl);
+    const orders = await updated.json();
+    setCart(orders);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Products</h2>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.id} - ${product.name}
+            <button onClick={() => handleAddToCart(product.id)}>
+              Add to cart
+            </button>
+          </li>
+        ))}
+      </ul>
+      <h2>Purchase</h2>
+      <ul>
+        {cart.map((product) => (
+                  <li key={product.id}>
+            {product.customer_id} - { product.product_id}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
